ae5260bc41e5c47481adcc71fb5d4f8667203c19
Kev-nDavis <119870131+Kev-nDavis@users.noreply.github.com>
2025-11-14 13:46:35 +0000

    Update menu-nav_showhide.js
    removed tl from chained events

--- menu-nav_showhide.js
+++ menu-nav_showhide.js
@@ -6,43 +6,43 @@ const mask = document.querySelector(".Body");
 
 const tl = gsap.timeline({ paused: true })
 
-  tl.from(".nav-layout_wrapper", {
+  .from(".nav-layout_wrapper", {
     backgroundColor: "transparent",
     ease: 'ease.out'
   })
   
-  tl.to(".nav-layout_wrapper", {
+  .to(".nav-layout_wrapper", {
     backgroundColor: "white",
     ease: 'ease.out'
   })
 
-	tl.from(".rwa-text", {
+.from(".rwa-text", {
    duration:0.1,
    color: "white"
   }, '-=<')
   
-  tl.to(".rwa-text", {
+  .to(".rwa-text", {
    duration:0.1,
    color: "var(--colours--rwg-slate)"
   }, '-=<')
 
-  tl.from(".group-text", {
+  .from(".group-text", {
     duration:0.1,
     color: "white"
   }, '-=<')
   
-  tl.to(".group-text", {
+  .to(".group-text", {
     duration:0.1,
     color: "var(--colours--rwg-sage)"
   }, '-=<')
 
-  tl.to("#burger-open", {
+  .to("#burger-open", {
     duration:0.5,
     ease: 'ease.out',
     color: "var(--colours--rwg-slate)"
   }, '-=<')
 
-  tl.to('#burger-label', {
+  .to('#burger-label', {
     duration:0.5,
     ease: 'ease.out',
     color: "var(--colours--rwg-slate)"
