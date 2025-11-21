1d5915c4b4e60e02b55f7d5c005b07f96ead7bfa
Kev-nDavis <119870131+Kev-nDavis@users.noreply.github.com>
2025-11-21 16:07:11 +0000

    Update menu-nav_main-rwa_v2.0.js

--- menu-nav_main-rwa_v2.0.js
+++ menu-nav_main-rwa_v2.0.js
@@ -86,14 +86,20 @@ document.addEventListener("DOMContentLoaded", () => {
         ease: 'ease.out',
         autoAlpha: 0
     })
- */       
+        
     .from(".exp-nav-btn__list", {
         duration: .3,
         xPercent: -80,
         ease: 'ease.out',
         autoAlpha: 0
     }, "-=0.2")
-
+*/
+    //new code copied in here
+    .fromTo(".exp-nav-btn__list", 
+          { xPercent: -80, autoAlpha: 0 }, // Start values
+          { duration: .3, xPercent: 0, autoAlpha: 1, ease: 'ease.out' }, // End values
+      "-=0.2")        
+        
     .addLabel("end-expertise_menu")    
     .addPause()
 
