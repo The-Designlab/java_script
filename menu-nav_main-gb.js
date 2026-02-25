1299ee407f9383d7abd83303e183d6a7a73a43f8
Kev-nDavis <119870131+Kev-nDavis@users.noreply.github.com>
2026-02-25 11:07:27 +0000

    Update menu-nav_main-gb.js
    removed delay that was previously put in

--- menu-nav_main-gb.js
+++ menu-nav_main-gb.js
@@ -163,7 +163,6 @@ document.addEventListener("DOMContentLoaded", () => {
     if (workBackBtn) {
         // QA Fix: Targetting the single, standardized label
         workBackBtn.addEventListener("click", function () {
-            gsap.delayedCall(0.5, () => {
             tl.play("nav-loaded");
         });
     }
