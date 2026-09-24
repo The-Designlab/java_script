202c66c03e1e3bbf92b9907dd0f71e49a029f3f8
Kev-nDavis <119870131+Kev-nDavis@users.noreply.github.com>
2026-09-24 12:31:45 +0100

    Create dtprod_case-study-cards_1.0.js

---
+++ dtprod_case-study-cards_1.0.js
@@ -0,0 +1,42 @@
+// timeline mouse enter and leave tween (image fade out, text reveal in) fixed to work with multiple objects using the same class of ".article-card_wrapper" to fire individually using one piece of code//
+
+const items = document.querySelectorAll(".environment-card_container");
+gsap.defaults({ defaults: { ease: 'Expo.easeInOut', duration: .2 } });
+
+items.forEach(function (item, index) {
+  const tl = gsap
+    .timeline({ paused: true })
+
+    /*.fromTo(item.querySelector(".ext-btn_wrapper"), {
+      color: "#ffffff"
+    }, {
+      opacity: 0
+    })*/
+
+    .to(item.querySelector('#read-more'), {
+      opacity: 0
+    })
+
+    .fromTo(item.querySelector('#card-cat'), {
+      color: "var(--_color---neutral--white)"
+    }, {
+      color: "#171f43",
+      ease: 'power3.in'
+    })
+
+    .to(item.querySelector(".mkt-card_image"), {
+      opacity: 0
+    }, "-=0.2")
+
+    .fromTo(item.querySelector(".mkt-card-body_text"), {
+      opacity: 0,
+      color: "var(--_color---neutral--white)"
+    }, {
+      opacity: 1,
+      color: "#171f43",
+      ease: 'power3.in'
+    }, "-=0.2");
+
+  item.addEventListener("mouseenter", () => tl.play());
+  item.addEventListener("mouseleave", () => tl.reverse());
+});
